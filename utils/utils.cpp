afee869bf8b46b785737665e68d0fc3ee8226e95
nzengi <zenginureddin1@gmail.com>
2024-08-25 09:50:01 +0300

    Create utils.hpp

---
+++ utils/utils.cpp
@@ -0,0 +1,14 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <string>
+
+namespace Utils {
+    // Function to check if a number is prime
+    bool isPrime(int num);
+
+    // Function to validate binary strings
+    bool validateBinaryString(const std::string& binary_str);
+}
+
+#endif // UTILS_HPP
